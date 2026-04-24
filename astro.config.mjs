8b9297b76d4a7ec7535920ef952ef32c875865f9
Matěj Račinský <racinsky.matej@seznam.cz>
2026-04-24 17:01:41 +0200

    init: copy from poc

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// Set site to your actual Cloudflare Pages URL or custom domain after first deploy.
+export default defineConfig({
+  site: 'https://<todo>.pages.dev',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  },
+});
