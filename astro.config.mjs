0ef2598372cc261f56dd91110e5f7883f80bf73b
Matěj Račinský <racinsky.matej@seznam.cz>
2026-04-24 17:18:56 +0200

    fix: url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // Set site to your actual Cloudflare Pages URL or custom domain after first deploy.
 export default defineConfig({
-  site: 'https://<todo>.pages.dev',
+  site: 'https://presentation-website-f4a.pages.dev',
   trailingSlash: 'always',
   build: {
     format: 'directory',
